7bdfb8420205c5913505f3f2903a8ef110f4b9eb
Kathirsaravanan <kathirsaravanan467@gmail.com>
2024-10-09 11:40:30 +0530

    Time: 66 ms (23.55%), Space: 49.2 MB (92.2%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -4,15 +4,26 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-    let arr = new Array(26);
-    arr.fill(0);
+    // let arr = new Array(26);
+    // arr.fill(0);
+    // for(let i=0;i<t.length;i++){
+    //     arr[t.charCodeAt(i)-97]++
+    //     arr[s.charCodeAt(i)-97]--
+    // }
+    // for(let i=0;i<arr.length;i++){
+    //     if(arr[i]>0){
+    //         return String.fromCodePoint(97+i);
+    //     }
+    // }
+    let sum1=0;
+    let sum2=0;
+    for(let i=0;i<s.length;i++){
+        sum1+=s.charCodeAt(i);
+
+    }
     for(let i=0;i<t.length;i++){
-        arr[t.charCodeAt(i)-97]++
-        arr[s.charCodeAt(i)-97]--
+        sum2+=t.charCodeAt(i);
+        
     }
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>0){
-            return String.fromCodePoint(97+i);
-        }
-    }
+    return String.fromCharCode(sum2-sum1);
 };
