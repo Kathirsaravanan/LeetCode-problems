834188cd8d787f7d65063483f73cfefb00d80786
Kathirsaravanan <kathirsaravanan467@gmail.com>
2024-10-09 11:06:21 +0530

    Time: 57 ms (66.5%), Space: 49.5 MB (88.69%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let arr = new Array(26);
+    arr.fill(0);
+    for(let i=0;i<t.length;i++){
+        arr[t.charCodeAt(i)-97]++
+    }
+    for(let i=0;i<s.length;i++){
+        arr[s.charCodeAt(i)-97]--
+    }
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>0){
+            return String.fromCodePoint(97+i);
+        }
+    }
+};
