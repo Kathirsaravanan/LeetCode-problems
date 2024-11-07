cfdde9a5dac1054afb742ef06891e5c3c3a7b8cd
Kathirsaravanan <kathirsaravanan467@gmail.com>
2024-11-07 13:23:44 +0530

    Time: 273 ms (91.18%), Space: 65 MB (82.81%) - LeetHub

--- 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.js
+++ 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var minPairSum = function(nums) {
-    nums=nums.sort((a,b)=>a-b);
+    nums=nums.sort((a,b)=> a-b);
     let n=nums.length;
     let max = 0;
     for(let i=0;i<nums.length/2;i++){
-        max= Math.max(max, nums[i]+nums[nums.length-i-1])
+        max= max>nums[i]+nums[n-1]?max:nums[i]+nums[n-1]
         n--;
     }
     return max;
