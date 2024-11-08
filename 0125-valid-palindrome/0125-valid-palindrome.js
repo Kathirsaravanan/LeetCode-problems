5f351403b5e6cdbd59ce09ca089a460ca802440b
Kathirsaravanan <kathirsaravanan467@gmail.com>
2024-11-08 08:46:54 +0530

    Time: 1 ms (98.77%), Space: 50.9 MB (94.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -6,13 +6,13 @@ var isPalindrome = function(s) {
     s=  s.toLowerCase().replace(/[^a-z0-9]/gi,'');
     let i=0;
     let j=s.length-1;
+    if(s.length<=1)return true;
     while(i<=j){
         if(s[i]!==s[j]){
             return false
-        }else if(s[i]===s[j]){
+        }
             i++;
             j--;
-        }
         
     }
     return true;
