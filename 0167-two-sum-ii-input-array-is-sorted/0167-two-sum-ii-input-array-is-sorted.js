60fc4ff348470ead4d417205cc6f4dacf84e4b3b
Kathirsaravanan <kathirsaravanan467@gmail.com>
2024-11-08 14:35:23 +0530

    Time: 5 ms (14.68%), Space: 50.2 MB (33.45%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    let i=0,j=nums.length;
+    while(i<=j){
+        const pairIndex = map.get(nums[i]);
+        if(pairIndex || pairIndex=== 0){
+            return [pairIndex+1,i+1]
+        }
+        const difference = target- nums[i];
+        map.set(difference,i);
+        i++;
+    }
+};
