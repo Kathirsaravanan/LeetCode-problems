d8530f7d50d123aef731ed4841d188c0e2c7b568
Kathirsaravanan <kathirsaravanan467@gmail.com>
2024-11-11 18:49:35 +0530

    Time: 1 ms (47.72%), Space: 56.8 MB (46.44%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -6,10 +6,9 @@ var reverseString = function(s) {
       let temp='';
       let i=0, j=s.length-1;
       while(i<j){
-        // temp=s[i];
-        // s[i]=s[j];
-        // s[j]=temp;
-        [s[i],s[j]]=[s[j],s[i]]
+        temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
         i++;
         j--;
       }
